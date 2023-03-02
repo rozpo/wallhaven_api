0827c42700e5eadcac5da0ad5375bea388bec483
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-03-02 11:33:08 +0100

    Create api client class

--- lib/wallhaven_api.dart
+++ lib/wallhaven_api.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// [Wallhaven API](https://wallhaven.cc) wrapper for the
+/// [Dart programming language](https://dart.dev/).
 library wallhaven_api;
 
-export 'src/wallhaven_api_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'api/wallhaven_api_client.dart';
