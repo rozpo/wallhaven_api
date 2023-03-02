913afa0de4ea33969d2940ba2dae59ddbd1e5c63
Piotr Rozpończyk <rozpo.dev@gmail.com>
2023-03-02 11:42:08 +0100

    Prepare base client functionality

--- lib/api/wallhaven_api_client.dart
+++ lib/api/wallhaven_api_client.dart
@@ -1,1 +1,25 @@
-class WallhavenApiClient {}
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class WallhavenApiClient {
+  static const String _baseUrl = 'https://wallhaven.cc/api/v1';
+
+  final Client _httpClient;
+
+  WallhavenApiClient({Client? httpClient})
+      : _httpClient = httpClient ?? Client();
+
+  void close() => _httpClient.close();
+
+  Future<Map<String, dynamic>> _request(Uri url) async {
+    final Response response = await _httpClient.get(url);
+
+    final Map<String, dynamic> data = jsonDecode(response.body);
+
+    if (response.statusCode != 200) {
+      // TODO throw exception
+    }
+    return data;
+  }
+}
